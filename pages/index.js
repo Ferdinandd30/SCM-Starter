9a0604d7601cf8f8e33579f894592fd2a0325ada
Ferdinandd30 <145783371+Ferdinandd30@users.noreply.github.com>
2023-10-05 03:50:09 +0000

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -5,11 +5,12 @@ import abi from "./contracts/abi.json";
 export default function HomePage() {
   const [ethWallet, setEthWallet] = useState(undefined);
   const [account, setAccount] = useState(undefined);
-  const [atm, setATM] = useState(undefined);
-  const [balance, setBalance] = useState(undefined);
+
+  const [contract, setContract] = useState(undefined);
 
   const contractAddress = "0x8088c3fD87f9E6EfF17CbD52f787A87E59f7D5B8";
-  const atmABI = atm_abi.abi;
+  const [milkBottles, setMilkBottless] = useState(undefined);
+  const [eggCrates, setEggCrates] = useState (undefined);
 
   const getWallet = async() => {
     if (window.ethereum) {
@@ -42,36 +43,43 @@ export default function HomePage() {
     handleAccount(accounts);
     
     // once wallet is set we can get a reference to our deployed contract
-    getATMContract();
+    getContract();
   };
 
-  const getATMContract = () => {
+  const getContract = () => {
     const provider = new ethers.providers.Web3Provider(ethWallet);
     const signer = provider.getSigner();
-    const atmContract = new ethers.Contract(contractAddress, atmABI, signer);
+    const IoT = new ethers.Contract(contractAddress, abi, signer);
  
-    setATM(atmContract);
+    setContract(IoT);
+
   }
 
-  const getBalance = async() => {
-    if (atm) {
-      setBalance((await atm.getBalance()).toNumber());
+  const getMilkBottles = async() => {
+    if (contract) {
+      setMilkBottless((await contract.milkBottles()).toNumber());
     }
   }
 
-  const deposit = async() => {
-    if (atm) {
-      let tx = await atm.deposit(1);
-      await tx.wait()
-      getBalance();
+  const getEggCrates = async() => {
+    if (contract) {
+      setEggCrates((await contract.eggCrates()).toNumber());
     }
   }
 
-  const withdraw = async() => {
-    if (atm) {
-      let tx = await atm.withdraw(1);
+  const getMilk = async() => {
+    if (contract) {
+      let tx = await contract.getOneMilk();
       await tx.wait()
-      getBalance();
+      getMilkBottles();
+    }
+  }
+
+  const getEggs = async() => {
+    if (contract) {
+      let tx = await contract.getOneEgg();
+      await tx.wait()
+      getEggCrates();
     }
   }
 
@@ -86,16 +94,24 @@ export default function HomePage() {
       return <button onClick={connectAccount}>Please connect your Metamask wallet</button>
     }
 
-    if (balance == undefined) {
-      getBalance();
+    if (eggCrates == undefined) {
+      getEggCrates();
     }
 
+    if (milkBottles == undefined) {
+      getMilkBottles();
+    }
+    
     return (
       <div>
         <p>Your Account: {account}</p>
-        <p>Your Balance: {balance}</p>
-        <button onClick={deposit}>Deposit 1 ETH</button>
-        <button onClick={withdraw}>Withdraw 1 ETH</button>
+        <p>Milk bottles in your fridge: {milkBottles}</p>
+        <p>Egg crates in your fridge: {eggCrates}</p>
+
+        <hr></hr>
+        <button onClick={getMilk}>get one milk</button>
+        <button onClick={getEggs}>get one eggs</button>
+        <hr></hr>
       </div>
     )
   }
@@ -104,7 +120,7 @@ export default function HomePage() {
 
   return (
     <main className="container">
-      <header><h1>Welcome to the Metacrafters ATM!</h1></header>
+      <header><h1>Welcome to the IoT website!</h1></header>
       {initUser()}
       <style jsx>{`
         .container {
